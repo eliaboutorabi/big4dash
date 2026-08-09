cabeba0351d7710cedaf9c395e6e893952354d8b
Elham Aboutorabi <eli.abotorabi@gmail.com>
2026-08-08 23:21:57 -0500

    fix GitHub Pages manifest path

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import { asset } from '$app/paths';
 	import './layout.css';
 	import favicon from '$lib/assets/favicon.svg';
 
@@ -7,7 +8,7 @@
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
-	<link rel="manifest" href="/site.webmanifest" />
+	<link rel="manifest" href={asset('/site.webmanifest')} />
 	<meta name="theme-color" content="#101722" />
 	<meta name="color-scheme" content="light dark" />
 </svelte:head>
